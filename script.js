d622515ba1af78555be673b5c37e9df68f0fec44
Edward Baldwin <ecbaldwin4+github@gmail.com>
2025-02-04 09:28:50 -0600

    trying again with the music and run away button

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function moveButton() {
     const noButton = document.getElementById('no-button');
     const x = Math.random() * (window.innerWidth - noButton.offsetWidth);
     const y = Math.random() * (window.innerHeight - noButton.offsetHeight);
-    noButton.style.position = 'absolute';
     noButton.style.left = `${x}px`;
     noButton.style.top = `${y}px`;
 }
